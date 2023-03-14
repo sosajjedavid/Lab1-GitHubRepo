3340f1471549515d9a64fdfa90454ff6dd115a7c
Joshua Jhon Emmanuel D. Sosa <sosajjedavid@gmail.com>
2023-03-14 16:27:31 -0400

    First Commit

---
+++ lib.js
@@ -0,0 +1,10 @@
+export function halfOf(x)
+{
+    return x / 2;
+}
+
+
+export function multiply(x, y)
+{
+    return x * y;
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+import {halfOf, multiply} from "./lib.js";
+import doSomething from "./doSomething.js";
+console.log(halfOf(84));
+console.log(multiply(21, 2));
+doSomething();
